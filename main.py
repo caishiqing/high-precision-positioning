c95ab2909c435b736943fdf0024faab6e3c43e37
caishiqing <1349053218@qq.com>
2022-08-07 10:01:54 +0800

    .

--- main.py
+++ main.py
@@ -8,18 +8,22 @@ import fire
 def train(data_file, label_file, save_path,
           pretrained_path=None, **kwargs):
 
-    print(kwargs)
-    batch_size = kwargs.pop('batch_size', 128)
-    infer_batch_size = kwargs.pop('infer_batch_size', batch_size)
-    epochs = kwargs.pop('epochs', 100)
-    learning_rate = kwargs.pop('learning_rate', 1e-3)
-    test_size = kwargs.pop('test_size', 0.2)
-
     x, y = load_data(data_file, label_file)
     x_train, x_valid, y_train, y_valid = train_test_split(x, y, test_size=test_size)
 
-    train_process = Process(target=TrainEngine(batch_size, infer_batch_size, epochs, learning_rate),
-                            args=((x_train, y_train), (x_valid, y_valid), save_path, pretrained_path))
+    train_engine = TrainEngine(batch_size=kwargs.pop('batch_size', 128),
+                               infer_batch_size=kwargs.pop('infer_batch_size', 128),
+                               epochs=kwargs.pop('epochs', 100),
+                               learning_rate=kwargs.pop('learning_rate', 1e-3),
+                               valid_augment_times=kwargs.pop('test_augment_times', 5))
+
+    train_process = Process(target=train_engine,
+                            args=(
+                                (x_train, y_train),
+                                (x_valid, y_valid),
+                                save_path,
+                                pretrained_path
+                            ))
     train_process.start()
     train_process.join()
 
