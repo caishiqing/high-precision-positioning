4e3262bdf1f1f2584c01100de27563ea7aaa85be
caishiqing <1349053218@qq.com>
2022-08-21 22:08:13 +0800

    .

--- train.py
+++ train.py
@@ -166,7 +166,7 @@ class PretrainEngine(TrainEngine):
                 print("Load pretrained weights from {}".format(pretrained_path))
                 model.load_weights(pretrained_path)
 
-            mbs_model.compile(optimizer=optimizer, loss=tf.keras.losses.mae)
+            mbs_model.compile(optimizer=optimizer, loss=tf.keras.losses.mse)
             mbs_model.summary()
             mbs_model.fit(x=train_data,
                           epochs=self.epochs,
