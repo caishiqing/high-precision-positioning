f0569c0b6438f0767b0b5c9737e2994dcca73b0e
caishiqing <1349053218@qq.com>
2022-09-02 22:16:12 +0800

    .

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from sklearn.model_selection import train_test_split
-from train import TrainEngine, load_data
+from train import TrainEngine, load_data, SemiTrainEngine
 from sklearn.decomposition import TruncatedSVD
 from modelDesign_1 import bs_masks as masks1
 from modelDesign_2 import bs_masks as masks2
@@ -44,6 +44,63 @@ def train(data_file,
                                bs_masks=bs_masks,
                                svd_weight=svd_weight,
                                loss_epsilon=kwargs.pop('loss_epsilon', 0.0))
+
+    # train_process = Process(target=train_engine,
+    #                         args=(
+    #                             (x_train, y_train),
+    #                             (x_valid, y_valid),
+    #                             repeat_data_times,
+    #                             pretrained_path,
+    #                             kwargs.pop('verbose', 1)
+    #                         ))
+
+    # train_process.start()
+    # train_process.join()
+
+    train_engine((x_train, y_train),
+                 (x_valid, y_valid),
+                 repeat_data_times,
+                 pretrained_path,
+                 kwargs.pop('verbose', 1))
+
+
+def semi_train(data_file,
+               label_file,
+               save_path,
+               pretrained_path=None,
+               mask_mode=1,
+               learn_svd=False,
+               repeat_data_times=1,
+               **kwargs):
+
+    tf.config.threading.set_inter_op_parallelism_threads(4)
+    x, y = load_data(data_file, label_file)
+    if learn_svd:
+        svd_weight = TruncatedSVD(256).fit(x.reshape([len(x) * 72, -1])).components_.T
+    else:
+        svd_weight = None
+
+    test_size = kwargs.pop('test_size', 0.1)
+    unlabel_x = x[len(y):]
+    x_train, x_valid, y_train, y_valid = train_test_split(
+        x[:len(y)], y / 120, test_size=test_size)
+
+    if mask_mode == 1:
+        bs_masks = masks1
+    elif mask_mode == 2:
+        bs_masks = masks2
+    print(bs_masks)
+
+    train_engine = SemiTrainEngine(save_path,
+                                   batch_size=kwargs.pop('batch_size', 128),
+                                   infer_batch_size=kwargs.pop('infer_batch_size', 128),
+                                   unlabel_data=unlabel_x,
+                                   epochs=kwargs.pop('epochs', 100),
+                                   learning_rate=kwargs.pop('learning_rate', 1e-3),
+                                   dropout=kwargs.pop('dropout', 0.0),
+                                   bs_masks=bs_masks,
+                                   svd_weight=svd_weight,
+                                   loss_epsilon=kwargs.pop('loss_epsilon', 0.0))
 
     # train_process = Process(target=train_engine,
     #                         args=(
