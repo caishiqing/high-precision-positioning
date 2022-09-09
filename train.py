55342d0622bf5acf16fc3cc56f999a75f038d6cc
caishiqing <1349053218@qq.com>
2022-09-09 22:26:00 +0800

    .

--- train.py
+++ train.py
@@ -197,9 +197,14 @@ class TrainEngine:
             #model.compile(optimizer=optimizer, loss=tf.keras.losses.mae)
             model.compile(optimizer=optimizer)
 
+            from scipy.spatial.distance import cdist
+
             y1 = model(valid_data[0][:256], training=True).numpy()
             y2 = model(valid_data[0][:256], training=True).numpy()
-            print(np.concatenate([y1, y2], 1))
+            dist = cdist(y1, y2)
+            p = np.diag(dist)
+            n = ndist[np.where(np.eye(256) == 0)]
+            print(p.max(), n.min())
 
             model.get_layer('wrapper').layer.summary()
             model.fit(x=train_dataset,
