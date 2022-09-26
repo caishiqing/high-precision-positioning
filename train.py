5fde1e5477c9fb9eb3ef8d6a9a312fb64738a347
caishiqing <1349053218@qq.com>
2022-09-27 00:06:45 +0800

    .

--- train.py
+++ train.py
@@ -75,7 +75,7 @@ def compare_loss(pos1, pos2):
 
     pd = dist[tf.equal(label, 1)]
     nd = dist[tf.equal(label, 0)]
-    threshold = 60.0 / tf.sqrt(tf.shape(pos1)[0] / 2 + 1e-5) / 10
+    threshold = tf.cast(60.0 / tf.sqrt(tf.shape(pos1)[0] / 2 + 1e-5) / 10, tf.float32)
     nd = tf.boolean_mask(nd, tf.less_equal(nd, threshold))
     loss = tf.math.log1p(tf.reduce_sum(pd)) + tf.math.log1p(tf.reduce_sum(1 / nd))
     return tf.reduce_mean(loss)
