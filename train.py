6c48078936a62c50f03fc5de19a98ead7e774dbb
caishiqing <1349053218@qq.com>
2022-09-28 02:29:25 +0800

    .

--- train.py
+++ train.py
@@ -103,13 +103,13 @@ def uniform_loss(pos, anchor_size=256, epsilon=1e-5):
     anchor = tf.cast(tf.linspace(0, 1, anchor_size), pos.dtype)
     pos = tf.keras.backend.flatten(pos)
     dist = tf.abs(tf.expand_dims(anchor, 0) - tf.expand_dims(pos, 1)) + epsilon
-    # loss = tf.reduce_sum((tf.nn.softmax(1/dist, 0) + tf.nn.softmax(1/dist, 1)) * dist)
+    loss = tf.reduce_sum((tf.nn.softmax(1/dist, 0) + tf.nn.softmax(1/dist, 1)) * dist)
     # dist = wasserstein_distance(anchor, pos)
-    gamma = tf.numpy_function(lambda *args: sinkhorn_stabilized(*args, reg=100, stopThr=1e-5, warn=False),
-                              inp=[pos, anchor, dist],
-                              Tout=tf.float32)
-    gamma /= tf.reduce_sum(gamma)
-    loss = tf.reduce_sum(gamma * dist)
+    # gamma = tf.numpy_function(lambda *args: sinkhorn_stabilized(*args, reg=100, stopThr=1e-5, warn=False),
+    #                           inp=[pos, anchor, dist],
+    #                           Tout=tf.float32)
+    # gamma /= tf.reduce_sum(gamma)
+    # loss = tf.reduce_sum(gamma * dist)
     return loss
 
 
